8e95b6ea985fc75fa13fb2b385e6eab6503fa549
AhmedEhabH <ahmed.ehab.h@gmail.com>
2023-10-21 17:31:15 +0300

    add product-item component add function of select by type or brand or both fix ui of products page add getBrands and getTypes in shop.service modify getProducts in shop.service add pipeline

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IProduct } from '../shared/models/product';
 import { ShopService } from './shop.service';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/productType';
 
 @Component({
   selector: 'app-shop',
@@ -9,14 +11,47 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent {
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  brandIdSelected: number = 0;
+  typeIdSelected: number = 0;
 
-  constructor(private shopService: ShopService){}
+  constructor(private shopService: ShopService) { }
 
-  ngOnInit(){
-    this.shopService.getProducts().subscribe({
-      next:(response)=>this.products = response.data,
-      error:(e)=>console.error(e)
-      
+  ngOnInit() {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe({
+      next: (response) => this.products = response.data,
+      error: (e) => console.error(e)
+    });
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe({
+      next: (response) => this.brands = [{ id: 0, name: 'All' }, ...response],
+      error: (e) => console.error(e)
     })
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe({
+      next: (response) => this.types = [{ id: 0, name: 'All' }, ...response],
+      error: (e) => console.error(e)
+    })
+  }
+
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts()
+  }
+
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts()
   }
 }
